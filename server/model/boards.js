f9af72f0c13a782733720ff2f1edbf0301e4ebab
journiyoon <journiyoon@gmail.com>
2022-05-09 23:57:58 +0900

    Add 'id' field

--- server/model/boards.js
+++ server/model/boards.js
@@ -3,10 +3,11 @@ const Schema = mongoose.Schema;
 const dummy = require("mongoose-dummy");
 
 const boardsSchema = new Schema(
-  {
-    title: String,
-  },
-  { timestamps: true }
+	{
+		id: { type: Number, required: true, unique: true },
+		title: { type: String, required: true, unique: true },
+	},
+	{ timestamps: true }
 );
 
 module.exports = mongoose.model("Boards", boardsSchema);

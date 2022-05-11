cbde4eadc0e4e52653a35666461bf0f5f4844c73
leesujin <jean0518@naver.com>
2022-05-12 02:16:16 +0900

    review success

--- server/controller/reviewController.js
+++ server/controller/reviewController.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 dotenv.config();
 const Charts = require("../model/charts");
-const reviews = require("../model/reviews");
+const Evaluations = require("../model/evaluations");
 
 module.exports = {
   //리뷰 전체 조회 핸들러
@@ -16,9 +16,12 @@ module.exports = {
           likes: true,
           username: true,
           updatedAt: true,
-          createdAt: true,
         }
-      ).populate("charts_id");
+      ).populate("charts_id", {
+        updatedAt: false,
+        createdAt: false,
+        __v: false,
+      });
 
       if (reviews) {
         res.status(200).send({ meesage: "리뷰 전체 조회 성공", data: reviews });
@@ -26,6 +29,7 @@ module.exports = {
         res.status(400).send({ message: "리뷰 전체 조회 실패" });
       }
     } catch (e) {
+      console.log(e);
       res.status(500).send({ message: "리뷰 전체 조회 실패" });
     }
   },
@@ -35,8 +39,8 @@ module.exports = {
     try {
       const id = req.params.reviewid;
       const review = await Reviews.findOne({ _id: id })
-        .populate("charts_id")
-        .populate("evaluations_id");
+        .populate("charts_id", { updatedAt: false, createdAt: false })
+        .populate("evaluations_id", { updatedAt: false, createdAt: false });
 
       if (review) {
         const data = {
@@ -60,10 +64,72 @@ module.exports = {
   },
 
   //리뷰 업데이트 핸들러
-  update: async (req, res) => {},
+  update: async (req, res) => {
+    try {
+      const id = req.params.reviewid;
+      const { body } = req.body;
+      const { popularity, artistry, lyrics, individuality, Addictive } =
+        req.body.evaluation;
+
+      const review = await Reviews.findOne({ _id: id }).populate(
+        "evaluations_id"
+      );
+      if (review) {
+        review.body = body;
+        review.evaluations_id.popularity = popularity;
+        review.evaluations_id.artistry = artistry;
+        review.evaluations_id.lyrics = lyrics;
+        review.evaluations_id.individuality = individuality;
+        review.evaluations_id.Addictive = Addictive;
+        await review.save();
+        res.status(201).send({ success: true, message: "리뷰 수정 성공" });
+      } else {
+        res.status(400).send({ success: false, message: "리뷰 수정 실패" });
+      }
+    } catch (e) {
+      console.log(e);
+      res.status(500).send({ message: "서버 오류" });
+    }
+  },
 
   //리뷰 작성 핸들러
-  create: async (req, res) => {},
+  create: async (req, res) => {
+    try {
+      const accessToken = req.cookies.accessToken;
+      const { title, body } = req.body;
+      const evaluation = req.body.evaluation;
+
+      if (!accessToken) {
+        res.status(400).send({ message: "accessToken not provided" });
+      } else if (accessToken === "invalidtoken") {
+        res
+          .status(400)
+          .send({ message: "invalid accesstoken, please login again" });
+      } else {
+        const userinfo = jwt.verify(accessToken, process.env.ACCESS_SECRET);
+        const charts_id = await Charts.findOne({ title });
+        // 평가항목을 추가하고 id 받아오기
+        const evaluations_id = await Evaluations.create(evaluation);
+
+        //쿠키에 있던 유저정보와 받아온 차트id, 평가id, 글내용 넣고 리뷰 생성
+        const newReview = await Reviews.create({
+          users_id: userinfo.id,
+          username: userinfo.username,
+          body: body,
+          charts_id: String(charts_id._id),
+          evaluations_id: String(evaluations_id._id),
+        });
+
+        if (newReview) {
+          res.status(201).send({ success: true, message: "리뷰 작성 성공" });
+        } else {
+          res.status(400).send({ success: false, message: "리뷰 작성 실패" });
+        }
+      }
+    } catch (e) {
+      res.status(500).send({ message: "서버 오류" });
+    }
+  },
 
   //댓글 작성 핸들러
   createComment: async (req, res) => {
@@ -95,9 +161,9 @@ module.exports = {
           }
         );
         if (comments) {
-          res.status(200).send({ message: "댓글 작성 성공" });
+          res.status(200).send({ success: true, message: "댓글 작성 성공" });
         } else {
-          res.status(400).send({ message: "댓글 작성 실패" });
+          res.status(400).send({ success: false, message: "댓글 작성 실패" });
         }
       }
     } catch (e) {

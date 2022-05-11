3889b428dbc965525e6a4adb7fe8979192e51969
journiyoon <journiyoon@gmail.com>
2022-05-11 17:34:59 +0900

    Modify schema

--- server/model/boards.js
+++ server/model/boards.js
@@ -3,8 +3,8 @@ const Schema = mongoose.Schema;
 
 const boardsSchema = new Schema(
 	{
-		id: { type: Number, required: true, unique: true },
 		title: { type: String, required: true, unique: true },
+		subtitle: { type: String, required: true, unique: true },
 	},
 	{ timestamps: true }
 );

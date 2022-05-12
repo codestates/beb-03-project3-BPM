1d1adf51bfd51b740dae39d9aaf389691fa9aa54
journiyoon <journiyoon@gmail.com>
2022-05-12 17:57:29 +0900

    Add total field

--- server/model/charts.js
+++ server/model/charts.js
@@ -2,13 +2,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const chartsSchema = new Schema(
-  {
-    image: { type: String, required: true },
-    title: { type: String, required: true },
-    artist: { type: String, required: true },
-    evaluations: { type: mongoose.Schema.Types.ObjectId, ref: "Evaluations" },
-  },
-  { timestamps: true }
+	{
+		image: { type: String, required: true },
+		title: { type: String, required: true },
+		artist: { type: String, required: true },
+		total: { type: Number },
+	},
+	{ timestamps: true }
 );
 
 module.exports = mongoose.model("Charts", chartsSchema);

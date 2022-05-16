845415111f5b8ebc7a58a59bf6ff3954063beaf2
leesujin <jean0518@naver.com>
2022-05-16 20:04:56 +0900

    contract

--- server/model/admin.js
+++ server/model/admin.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const adminSchema = new Schema({
   address: String,
   privatekey: String,
+  title: String,
 });
 
 module.exports = mongoose.model("Admin", adminSchema);

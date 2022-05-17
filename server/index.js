e6e59749b443e1b90724c628d4bd2b2ad23a3b70
journiyoon <journiyoon@gmail.com>
2022-05-18 02:37:33 +0900

    add mypageRouter

--- server/index.js
+++ server/index.js
@@ -13,20 +13,21 @@ const reviewRouter = require("./router/reviewRouter");
 const chartRouter = require("./router/chartRouter");
 const contractRouter = require("./router/contractRouter");
 const columnRouter = require("./router/columnRouter");
+const mypageRouter = require("./router/mypageRouter");
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(
-  cors({
-    origin: ["http://localhost:3000"],
-    credentials: true,
-    methods: ["GET", "POST", "OPTIONS", "PATCH", "DELETE"],
-  })
+	cors({
+		origin: ["http://localhost:3000"],
+		credentials: true,
+		methods: ["GET", "POST", "OPTIONS", "PATCH", "DELETE"],
+	})
 );
 
 app.get("/", (req, res) => {
-  res.send("BPM Backend Server");
+	res.send("BPM Backend Server");
 });
 
 app.use("/user", userRouter);
@@ -34,22 +35,23 @@ app.use("/main", mainRouter);
 app.use("/board", boardRouter);
 app.use("/post", postRouter);
 app.use("/review", reviewRouter);
+app.use("/mypage", mypageRouter);
 // 차트 생성
 app.use("/chart", chartRouter);
 app.use("/contract", contractRouter);
 app.use("/column", columnRouter);
 //mongoose 연결
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  })
-  .then((res) => {
-    console.log("success mongoose start");
-  })
-  .catch((e) => {
-    console.log(e);
-  });
+	.connect(process.env.MONGODB_URI, {
+		useUnifiedTopology: true,
+		useNewUrlParser: true,
+	})
+	.then((res) => {
+		console.log("success mongoose start");
+	})
+	.catch((e) => {
+		console.log(e);
+	});
 
 server = app.listen(4000);
 console.log("http server runnning!!");

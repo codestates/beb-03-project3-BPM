64035f718ec515cd312982c32d2ec4cb62698eeb
yoon-asha <yoon.asha97@gmail.com>
2022-05-19 15:28:18 +0900

    board, music 작업 중

--- client/src/components/myComponents/MyComment.tsx
+++ client/src/components/myComponents/MyComment.tsx
@@ -1,12 +1,12 @@
 import {
-	ListItem,
-	TableContainer,
-	Table,
-	TableHead,
-	TableRow,
-	TableBody,
-	TableCell,
-	Typography,
+  ListItem,
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableBody,
+  TableCell,
+  Typography,
 } from "@mui/material";
 import { Link } from "react-router-dom";
 import axios from "axios";
@@ -14,62 +14,62 @@ import { log } from "console";
 import { Async } from "react-async";
 
 export default function MyComment() {
-	async function Comments() {
-		const comment = await axios.get("http://localhost:4000/mypage/comments", {
-			withCredentials: true,
-		});
-		return comment.data.data;
-	}
+  async function Comments() {
+    const comment = await axios.get("http://localhost:4000/mypage/comments", {
+      withCredentials: true,
+    });
+    return comment.data.data;
+  }
 
-	return (
-		<>
-			<TableContainer>
-				<Table>
-					<TableHead>
-						<TableRow>
-							<TableCell align="center">번호</TableCell>
-							<TableCell align="center">내용</TableCell>
-							<TableCell align="center">작성일</TableCell>
-						</TableRow>
-					</TableHead>
-					<TableBody>
-						<Async promiseFn={Comments}>
-							{({ data, error, isPending }) => {
-								if (isPending)
-									return (
-										<>
-											<Typography m="100px">Loding....</Typography>
-										</>
-									);
-								if (error) return `Something went wrong: ${error.message}`;
-								return (
-									<>
-										{data.map((comment: any, index: number) => {
-											return (
-												<>
-													<TableRow
-														component={Link}
-														to={`/community/${comment.boards_id}/${comment._id}`}
-														sx={{ textDecoration: "none" }}
-													>
-														<TableCell align="center">{index + 1}</TableCell>
-														<TableCell align="left">
-															{comment.comments.body}
-														</TableCell>
-														<TableCell align="center">
-															{comment.comments.createdAt.slice(0, 10)}
-														</TableCell>
-													</TableRow>
-												</>
-											);
-										})}
-									</>
-								);
-							}}
-						</Async>
-					</TableBody>
-				</Table>
-			</TableContainer>
-		</>
-	);
+  return (
+    <>
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">번호</TableCell>
+              <TableCell align="center">내용</TableCell>
+              <TableCell align="center">작성일</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <Async promiseFn={Comments}>
+              {({ data, error, isPending }) => {
+                if (isPending)
+                  return (
+                    <>
+                      <Typography m="100px">Loding....</Typography>
+                    </>
+                  );
+                if (error) return `Something went wrong: ${error.message}`;
+                return (
+                  <>
+                    {data.map((comment: any, index: number) => {
+                      return (
+                        <>
+                          <TableRow
+                            component={Link}
+                            to={`/community/${comment.boards_id}/${comment._id}`}
+                            sx={{ textDecoration: "none" }}
+                          >
+                            <TableCell align="center">{index + 1}</TableCell>
+                            <TableCell align="left">
+                              {comment.comments.body}
+                            </TableCell>
+                            <TableCell align="center">
+                              {comment.comments.createdAt.slice(0, 10)}
+                            </TableCell>
+                          </TableRow>
+                        </>
+                      );
+                    })}
+                  </>
+                );
+              }}
+            </Async>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
 }
--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -1,243 +1,191 @@
 import {
-<<<<<<< HEAD
   Container,
   CssBaseline,
-  Avatar,
+  Stack,
   Typography,
   Box,
   Grid,
   TextField,
   Button,
-} from '@mui/material';
-import { useNavigate } from 'react-router-dom';
-import React from 'react';
-import axios from 'axios';
-=======
-	Container,
-	CssBaseline,
-	Avatar,
-	Typography,
-	Box,
-	Grid,
-	TextField,
-	Button,
 } from "@mui/material";
-import { useNavigate } from "react-router-dom";
-import React from "react";
 import axios from "axios";
->>>>>>> 2b701b83f2008d63bc0bc0d40b342c4a510bbb15
+import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
 
 interface propstype {
-	account: string;
-	setIsLogin: any;
+  account: string;
+  setIsLogin: any;
 }
 
 export default function SignUp({ account, setIsLogin }: propstype) {
-	const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [validation, setValidation] = useState("");
 
-<<<<<<< HEAD
   const handleSubmit = (event: any) => {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
-
-    axios
-      .post('http://localhost:4000/user/signup', {
-        address: account,
-        username: formData.get('username'),
-        email: formData.get('email'),
-      })
-      .then(() => {
-        setIsLogin(true);
-        navigate('/signin');
-      })
-      .catch((e) => {
-        console.log(e);
-      });
+    if (formData.get("username") === "") {
+      setValidation("닉네임을 입력해주세요");
+    } else if (formData.get("email") === "") {
+      setValidation("이메일을 입력해주세요");
+    } else {
+      axios
+        .post("http://localhost:4000/user/signup", {
+          address: account,
+          username: formData.get("username"),
+          email: formData.get("email"),
+        })
+        .then((res) => {
+          setIsLogin(true);
+          setValidation("");
+          navigate("/signin");
+        })
+        .catch((e) => {
+          console.log(e.message);
+          if (e.message === "Request failed with status code 404") {
+            setValidation("로그인부터 다시 진행해주세요");
+          } else if (e.message === "Request failed with status code 400") {
+            setValidation("중복된 닉네임 입니다.");
+          }
+        });
+    }
   };
-  return (
-    <>
-      <Box
-        sx={{
-          bgcolor: '#111',
-          height: '100vh',
-        }}
-      >
-        metamask login 만약 없는 유저면 이 회원가입 페이지로 오는데 닉네임이랑
-        이메일 입력하는 칸 만들고 SignUp sumbmit!
-        <Container sx={{ bgcolor: '#111' }}>
-          <CssBaseline />
-          <Box
-            sx={{
-              marginTop: 8,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            {/* <Avatar sx={{ m: 1, bgcolor: 'success.main' }}>
-=======
-	const handleSubmit = (event: any) => {
-		event.preventDefault();
-		const formData = new FormData(event.currentTarget);
 
-		axios
-			.post("http://localhost:4000/user/signup", {
-				address: account,
-				username: formData.get("username"),
-				email: formData.get("email"),
-			})
-			.then(() => {
-				setIsLogin(true);
-				navigate("/signin");
-			})
-			.catch((e) => {
-				console.log(e);
-			});
-	};
-	return (
-		<>
-			<Box
-				sx={{
-					bgcolor: "#111",
-					height: "100vh",
-				}}
-			>
-				metamask login 만약 없는 유저면 이 회원가입 페이지로 오는데 닉네임이랑
-				이메일 입력하는 칸 만들고 SignUp sumbmit!
-				<Container sx={{ bgcolor: "#111" }}>
-					<CssBaseline />
-					<Box
-						sx={{
-							marginTop: 8,
-							display: "flex",
-							flexDirection: "column",
-							alignItems: "center",
-						}}
-					>
-						{/* <Avatar sx={{ m: 1, bgcolor: 'success.main' }}>
->>>>>>> 2b701b83f2008d63bc0bc0d40b342c4a510bbb15
-            <LockOutlinedIcon />
-          </Avatar>
+  return (
+    <Stack
+      sx={{
+        bgcolor: "#111",
+        height: "100vh",
+      }}
+    >
+      <Container sx={{ bgcolor: "#111" }}>
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
           <Typography
-            component='h1'
-            variant='h5'
-            sx={{ fontFamily: 'Nanum Gothic' }}
+            component="h1"
+            fontSize="5rem"
+            // fontFamily='Copperplate'
+            // fontFamily='Papyrus'
+            // fontFamily='Lucida Handwriting'
+            // fontFamily='Monaco'
+            // fontFamily='cursive'
+            // fontFamily='fantasy'
+            // fontFamily='Tahoma'
+            // fontFamily='Teko'
+            // fontFamily='Play'
+            // fontFamily='Great Vibes'
+            fontFamily="Sacramento"
+            sx={{
+              color: "#fff",
+              textAlign: "center",
+            }}
+            mb={10}
           >
-            회원가입
-          </Typography> */}
-						<Typography
-							component="h1"
-							fontSize="5rem"
-							// fontFamily='Copperplate'
-							// fontFamily='Papyrus'
-							// fontFamily='Lucida Handwriting'
-							// fontFamily='Monaco'
-							// fontFamily='cursive'
-							// fontFamily='fantasy'
-							// fontFamily='Tahoma'
-							// fontFamily='Teko'
-							// fontFamily='Play'
-							// fontFamily='Great Vibes'
-							fontFamily="Sacramento"
-							sx={{
-								color: "#fff",
-								textAlign: "center",
-							}}
-							mb={10}
-						>
-							Better Paricipation in Music
-						</Typography>
-						<Box
-							component="form"
-							noValidate
-							onSubmit={handleSubmit}
-							sx={{ mt: 3 }}
-						>
-							<Grid container spacing={2}>
-								<Grid item xs={12}>
-									<TextField
-										autoComplete="given-name"
-										name="username"
-										required
-										fullWidth
-										id="username"
-										label="닉네임"
-										autoFocus
-										sx={{
-											bgcolor: "darkviolet",
-											"& label.Mui-focused": {
-												color: "yellow",
-											},
-											"& .MuiInput-underline:after": {
-												borderBottomColor: "yellow",
-											},
-											"& .MuiOutlinedInput-root": {
-												"& fieldset": {
-													// borderColor: 'white',
-												},
-												"&:hover fieldset": {
-													borderColor: "white",
-												},
-												"&.Mui-focused fieldset": {
-													borderColor: "yellow",
-												},
-											},
-										}}
-									/>
-								</Grid>
-								<Grid item xs={12}>
-									<TextField
-										required
-										fullWidth
-										id="email"
-										label="이메일"
-										name="email"
-										autoComplete="email"
-										sx={{
-											bgcolor: "darkviolet",
-											"& label.Mui-focused": {
-												color: "yellow",
-											},
-											"& .MuiInput-underline:after": {
-												borderBottomColor: "yellow",
-											},
-											"& .MuiOutlinedInput-root": {
-												"& fieldset": {
-													// borderColor: 'white',
-												},
-												"&:hover fieldset": {
-													borderColor: "white",
-												},
-												"&.Mui-focused fieldset": {
-													borderColor: "yellow",
-												},
-											},
-										}}
-									/>
-								</Grid>
-							</Grid>
-							<Button
-								type="submit"
-								fullWidth
-								variant="contained"
-								sx={{
-									mt: 3,
-									mb: 2,
-									p: 2,
-									fontSize: "1.2rem",
-									bgcolor: "darkviolet",
-									"&: hover": {
-										bgcolor: "white",
-										color: "#000",
-									},
-								}}
-							>
-								회원가입
-							</Button>
-						</Box>
-					</Box>
-				</Container>
-			</Box>
-		</>
-	);
+            Better Paricipation in Music
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3, border: 1 }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  autoComplete="given-name"
+                  name="username"
+                  required
+                  fullWidth
+                  id="username"
+                  label="닉네임"
+                  autoFocus
+                  sx={{
+                    bgcolor: "darkviolet",
+                    "& label.Mui-focused": {
+                      color: "yellow",
+                    },
+                    "& .MuiInput-underline:after": {
+                      borderBottomColor: "yellow",
+                    },
+                    "& .MuiOutlinedInput-root": {
+                      "& fieldset": {
+                        // borderColor: 'white',
+                      },
+                      "&:hover fieldset": {
+                        borderColor: "white",
+                      },
+                      "&.Mui-focused fieldset": {
+                        borderColor: "yellow",
+                      },
+                    },
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="이메일"
+                  name="email"
+                  autoComplete="email"
+                  sx={{
+                    bgcolor: "darkviolet",
+                    "& label.Mui-focused": {
+                      color: "yellow",
+                    },
+                    "& .MuiInput-underline:after": {
+                      borderBottomColor: "yellow",
+                    },
+                    "& .MuiOutlinedInput-root": {
+                      "& fieldset": {
+                        // borderColor: 'white',
+                      },
+                      "&:hover fieldset": {
+                        borderColor: "white",
+                      },
+                      "&.Mui-focused fieldset": {
+                        borderColor: "yellow",
+                      },
+                    },
+                  }}
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{
+                mt: 3,
+                mb: 2,
+                p: 2,
+                fontSize: "1.2rem",
+                bgcolor: "darkviolet",
+                "&: hover": {
+                  bgcolor: "white",
+                  color: "#000",
+                },
+              }}
+            >
+              회원가입
+            </Button>
+
+            <Grid>
+              <Typography sx={{ color: "red", fontSize: 20, mt: 2 }}>
+                {validation}
+              </Typography>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </Stack>
+  );
 }

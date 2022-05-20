7ab3c98e12fa53f28d60a930393de4a994491c7b
yoon-asha <yoon.asha97@gmail.com>
2022-05-20 14:52:13 +0900

    post write 작업 중

--- client/src/components/commuComponents/PostWrite.tsx
+++ client/src/components/commuComponents/PostWrite.tsx
@@ -1,5 +1,25 @@
-import React from "react";
+import * as React from "react";
+import { Box, Divider, TextField, Typography } from "@mui/material";
 
 export default function PostWrite() {
-  return <>hello post write speed modal 클릭시 여기로 오기</>;
+  return (
+    <>
+      <Box p={5}>
+        <Typography></Typography>
+        <TextField
+          autoFocus
+          id="title"
+          label="제목"
+          type="title"
+          fullWidth
+          variant="standard"
+          placeholder="글 제목을 작성해주세요"
+          sx={{}}
+        />
+
+        <Divider />
+        <TextField fullWidth />
+      </Box>
+    </>
+  );
 }

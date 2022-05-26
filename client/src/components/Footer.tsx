d64582cf5ec41de9615ec37a00b20536741dff4e
yoon-asha <yoon.asha97@gmail.com>
2022-05-26 14:01:03 +0900

    Chart 정렬, Footer 구현

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,12 +1,52 @@
-import { Box } from "@mui/system";
+import { Container, Box, IconButton, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 import React from "react";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 export default function Footer() {
   return (
     <>
-      <Box sx={{ bgcolor: "lightgray", p: 15, textAlign: "center", mt: 10 }}>
-        이것은 푸터입니당 여기엔 깃허브, 개인 sns 등 정보가 들어갈 예정입니당
-      </Box>
+      <Container sx={{ bgcolor: "", p: 5, textAlign: "center", mt: 10 }}>
+        <Box>
+          <Typography variant="h2" fontFamily="Great Vibes">
+            Better Participation in Music
+          </Typography>
+          <Typography mt={2}>
+            기존 음원 순위에 대한 평가 검증 시스템을 갖춘 뮤지션들의 커뮤니티
+          </Typography>
+          <Box display="flex" justifyContent="center">
+            <Typography color="coral" mr={1}>
+              기획 |{" "}
+            </Typography>
+            <Typography>김민성</Typography>
+            <Typography color="lightcoral" ml={3} mr={1}>
+              개발 |
+            </Typography>
+            <Typography>
+              <a
+                href="https://yoon-asha.tistory.com/"
+                style={{ textDecoration: "none", color: "#111" }}
+              >
+                윤다미
+              </a>{" "}
+              윤지연 이수진
+            </Typography>
+          </Box>
+        </Box>
+        <IconButton href="https://github.com/codestates/beb-03-project3-BPM.git">
+          <GitHubIcon sx={{ color: "#333" }} />
+        </IconButton>
+        <IconButton
+          sx={{ width: 45 }}
+          href="https://www.notion.so/Better-Participation-in-Music-777081791ad945c5ad036c32d56ad6f8"
+        >
+          <img
+            src="https://img.icons8.com/color/48/000000/notion--v1.png"
+            style={{ width: "100%" }}
+          />
+        </IconButton>
+        <Typography>Design by 2022.Asha</Typography>
+      </Container>
     </>
   );
 }
--- client/src/pages/Chart.tsx
+++ client/src/pages/Chart.tsx
@@ -8,10 +8,13 @@ import {
   TableBody,
   TableCell,
   Typography,
+  Grid,
+  Box,
 } from "@mui/material";
 import PlayCircleOutlineIcon from "@mui/icons-material/PlayCircleOutline";
 import axios from "axios";
 import { Async } from "react-async";
+import { borderTop } from "@mui/system";
 
 export default function Chart() {
   async function getOurChart() {
@@ -25,168 +28,225 @@ export default function Chart() {
   }
   return (
     <>
-      <TableContainer sx={{ display: "flex" }}>
-        <Table sx={{ mr: 10 }}>
-          <TableHead>
-            <TableRow>
-              <TableCell align="center" sx={{ whiteSpace: "nowrap" }}>
-                순위
-              </TableCell>
-              <TableCell align="center">곡정보</TableCell>
-              <TableCell align="center">아티스트</TableCell>
-              <TableCell align="center">별점</TableCell>
-              <TableCell align="center">듣기</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            <Async promiseFn={getOurChart}>
-              {({ data, error, isPending }) => {
-                if (isPending)
-                  return (
-                    <>
-                      <Typography m="100px">Loding....</Typography>
-                    </>
-                  );
-                if (error) return `Something went wrong: ${error.message}`;
-                return (
-                  <>
-                    {data.map((chartData: any, index: number) => {
+      <Grid container p={5} spacing={5}>
+        <Grid item lg={7} md={12}>
+          {/* 우리 차트 */}
+          <Typography
+            textAlign="center"
+            variant="h3"
+            my={10}
+            // fontFamily="Copperplate"
+            // fontFamily="Papyrus"
+            // fontFamily="Lucida Handwriting"
+            // fontFamily="Monaco"
+            // fontFamily="cursive"
+            // fontFamily="fantasy"
+            // fontFamily="Tahoma"
+            // fontFamily="Teko"
+            fontFamily="Play"
+            // fontFamily="Great Vibes"
+            // fontFamily="Sacramento"
+          >
+            <span
+              style={{
+                fontFamily: "Play",
+                color: "lightcoral",
+                fontWeight: 600,
+              }}
+            >
+              BPM
+            </span>{" "}
+            Chart
+          </Typography>
+          <TableContainer
+            sx={{
+              border: "1px solid lightcoral",
+              borderRadius: 20,
+              p: 5,
+            }}
+          >
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell align="center" sx={{ whiteSpace: "nowrap" }}>
+                    순위
+                  </TableCell>
+                  <TableCell align="center">곡정보</TableCell>
+                  <TableCell align="center">아티스트</TableCell>
+                  <TableCell align="center">별점</TableCell>
+                  <TableCell align="center">듣기</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <Async promiseFn={getOurChart}>
+                  {({ data, error, isPending }) => {
+                    if (isPending)
                       return (
                         <>
-                          <TableRow>
-                            <TableCell>{index + 1}</TableCell>
-                            <TableCell
-                              scope="row"
-                              sx={{
-                                fontWeight: "550",
-                                color: "#333",
-                                "&:hover": {
-                                  cursor: "pointer",
-                                  color: "purple",
-                                  transition: "color .2s",
-                                },
-                                display: "flex",
-                                alignItems: "center",
-                              }}
-                            >
-                              <Link
-                                to={`${chartData._id}`}
-                                style={{
-                                  textDecoration: "none",
-                                  color: "black",
-                                }}
-                              >
-                                <img
-                                  src={chartData.image}
-                                  style={{
-                                    width: 40,
-                                    border: "1px solid #0002",
-                                    margin: "0 20px 0 30px",
-                                  }}
-                                />
-                              </Link>
-                              {chartData.title}
-                            </TableCell>
-                            <TableCell align="center">
-                              {chartData.artist}
-                            </TableCell>
-                            <TableCell align="center">
-                              {chartData.total}
-                            </TableCell>
-                            <TableCell align="center">
-                              <Button
-                                href={`${chartData.src}`}
-                                style={{ zIndex: 2000 }}
-                              >
-                                <PlayCircleOutlineIcon
-                                  sx={{ color: "#0009" }}
-                                />
-                              </Button>
-                            </TableCell>
-                          </TableRow>
+                          <Typography m="100px">Loding....</Typography>
                         </>
                       );
-                    })}
-                  </>
-                );
+                    if (error) return `Something went wrong: ${error.message}`;
+                    return (
+                      <>
+                        {data.map((chartData: any, index: number) => {
+                          return (
+                            <>
+                              <TableRow>
+                                <TableCell>{index + 1}</TableCell>
+                                <TableCell>
+                                  <Box
+                                    component={Link}
+                                    to={`${chartData._id}`}
+                                    sx={{
+                                      textDecoration: "none",
+                                      display: "flex",
+                                      alignItems: "center",
+                                      fontWeight: "600",
+                                      color: "#333",
+                                      "&:hover": {
+                                        cursor: "pointer",
+                                        color: "purple",
+                                        transition: "color .2s",
+                                      },
+                                    }}
+                                  >
+                                    <img
+                                      src={chartData.image}
+                                      style={{
+                                        width: 40,
+                                        border: "1px solid #0002",
+                                        margin: "0 20px 0 30px",
+                                      }}
+                                    />
+                                    {chartData.title}
+                                  </Box>
+                                </TableCell>
+                                <TableCell align="center">
+                                  {chartData.artist}
+                                </TableCell>
+                                <TableCell align="center">
+                                  {chartData.total}
+                                </TableCell>
+                                <TableCell align="center">
+                                  <Button
+                                    href={`${chartData.src}`}
+                                    style={{ zIndex: 2000 }}
+                                  >
+                                    <PlayCircleOutlineIcon
+                                      sx={{ color: "#0009" }}
+                                    />
+                                  </Button>
+                                </TableCell>
+                              </TableRow>
+                            </>
+                          );
+                        })}
+                      </>
+                    );
+                  }}
+                </Async>
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
+        <Grid item lg={5} md={12}>
+          {/* 멜론 차트 */}
+          <Typography
+            textAlign="center"
+            variant="h3"
+            my={10}
+            fontFamily="Play"
+            color="#666"
+            sx={{
+              mt: { md: 20, lg: 10 },
+              pt: { md: 15, lg: 0 },
+              borderTop: { md: "1px dashed lightgreen", lg: "none" },
+            }}
+          >
+            <span
+              style={{
+                fontFamily: "Play",
+                color: "#1fcf62",
               }}
-            </Async>
-          </TableBody>
-        </Table>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell align="center" sx={{ whiteSpace: "nowrap" }}>
-                순위
-              </TableCell>
-              <TableCell align="center">곡정보</TableCell>
-              <TableCell align="center">아티스트</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            <Async promiseFn={getAllChart}>
-              {({ data, error, isPending }) => {
-                if (isPending)
-                  return (
-                    <>
-                      <Typography m="100px">Loding....</Typography>
-                    </>
-                  );
-                if (error) return `Something went wrong: ${error.message}`;
-                return (
-                  <>
-                    {data.map((chartData: any, index: number) => {
+            >
+              Melon
+            </span>
+            &nbsp;Chart
+          </Typography>
+          <TableContainer
+            sx={{
+              border: "1px solid #1fcf62",
+              borderRadius: 20,
+              p: 5,
+            }}
+          >
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell align="center" sx={{ whiteSpace: "nowrap" }}>
+                    순위
+                  </TableCell>
+                  <TableCell align="center">곡정보</TableCell>
+                  <TableCell align="center">아티스트</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <Async promiseFn={getAllChart}>
+                  {({ data, error, isPending }) => {
+                    if (isPending)
                       return (
                         <>
-                          <TableRow>
-                            <TableCell>{index + 1}</TableCell>
-                            <TableCell
-                              scope="row"
-                              sx={{
-                                fontWeight: "550",
-                                color: "#333",
-
-                                display: "flex",
-                                alignItems: "center",
-                              }}
-                            >
-                              <Link
-                                to={`${chartData._id}`}
-                                style={{
-                                  textDecoration: "none",
-                                  color: "black",
-                                }}
-                              >
-                                <img
-                                  src={chartData.image}
-                                  style={{
-                                    width: 40,
-                                    border: "1px solid #0002",
-
-                                    margin: "0 20px 0 30px",
-                                  }}
-                                />
-                              </Link>
-                              {chartData.title}
-                            </TableCell>
-                            <TableCell align="center">
-                              {chartData.artist}
-                            </TableCell>
-                            <TableCell align="center"></TableCell>
-                            <TableCell align="center">
-                              {chartData.likes}
-                            </TableCell>
-                          </TableRow>
+                          <Typography m="100px">Loding....</Typography>
                         </>
                       );
-                    })}
-                  </>
-                );
-              }}
-            </Async>
-          </TableBody>
-        </Table>
-      </TableContainer>
+                    if (error) return `Something went wrong: ${error.message}`;
+                    return (
+                      <>
+                        {data.map((chartData: any, index: number) => {
+                          return (
+                            <>
+                              <TableRow>
+                                <TableCell>{index + 1}</TableCell>
+                                <TableCell>
+                                  <Box
+                                    component={Link}
+                                    to={`${chartData._id}`}
+                                    sx={{
+                                      fontWeight: "550",
+                                      color: "#333",
+                                      display: "flex",
+                                      alignItems: "center",
+                                      textDecoration: "none",
+                                    }}
+                                  >
+                                    <img
+                                      src={chartData.image}
+                                      style={{
+                                        width: 40,
+                                        border: "1px solid #0002",
+                                        margin: "0 20px 0 30px",
+                                      }}
+                                    />
+                                    {chartData.title}
+                                  </Box>
+                                </TableCell>
+                                <TableCell align="center">
+                                  {chartData.artist}
+                                </TableCell>
+                              </TableRow>
+                            </>
+                          );
+                        })}
+                      </>
+                    );
+                  }}
+                </Async>
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
+      </Grid>
     </>
   );
 }

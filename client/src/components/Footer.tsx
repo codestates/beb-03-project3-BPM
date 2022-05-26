7ec3ccda2b975c4fb03700ecda292420bd7019f3
yoon-asha <yoon.asha97@gmail.com>
2022-05-26 14:17:07 +0900

    Footer 링크 추가, target blank 추가

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -18,18 +18,39 @@ export default function Footer() {
             <Typography color="coral" mr={1}>
               기획 |{" "}
             </Typography>
-            <Typography>김민성</Typography>
+            <Typography>
+              <a
+                href="https://velog.io/@dappfer_eth"
+                target="_blank"
+                style={{ textDecoration: "none", color: "#111" }}
+              >
+                김민성
+              </a>
+            </Typography>
             <Typography color="lightcoral" ml={3} mr={1}>
               개발 |
             </Typography>
             <Typography>
               <a
                 href="https://yoon-asha.tistory.com/"
+                target="_blank"
                 style={{ textDecoration: "none", color: "#111" }}
               >
                 윤다미
               </a>{" "}
-              윤지연 이수진
+              <a
+                href="https://github.com/journiyoon"
+                target="_blank"
+                style={{ textDecoration: "none", color: "#111" }}
+              ></a>
+              윤지연{" "}
+              <a
+                href="https://github.com/sujin96"
+                target="_blank"
+                style={{ textDecoration: "none", color: "#111" }}
+              >
+                이수진
+              </a>
             </Typography>
           </Box>
         </Box>
@@ -37,12 +58,24 @@ export default function Footer() {
           <GitHubIcon sx={{ color: "#333" }} />
         </IconButton>
         <IconButton
-          sx={{ width: 45 }}
+          sx={{ width: 43 }}
+          target="_blank"
           href="https://www.notion.so/Better-Participation-in-Music-777081791ad945c5ad036c32d56ad6f8"
         >
           <img
             src="https://img.icons8.com/color/48/000000/notion--v1.png"
             style={{ width: "100%" }}
+          />
+        </IconButton>
+        <IconButton
+          sx={{ width: 45 }}
+          href="
+        https://discord.gg/TH8UYGja"
+          target="_blank"
+        >
+          <img
+            style={{ width: "100%" }}
+            src="https://img.icons8.com/ios-filled/50/000000/discord-logo.png"
           />
         </IconButton>
         <Typography>Design by 2022.Asha</Typography>
